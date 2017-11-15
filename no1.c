8078f33d192fab0e2b04876fbb26ea93cdeeba49
nuzhamusyafira <nuzhamusyafira@gmail.com>
2017-11-15 16:09:04 +0700

    rev

--- no1.c
+++ no1.c
@@ -8,22 +8,13 @@
 #include <errno.h>
 #include <sys/time.h>
 
-static const char *dirpath = "/home/tasha/Documents";
+static const char *dirpath = "/home/nuzha/Documents";
 
 static int xmp_getattr(const char *path, struct stat *stbuf)
 {
-	int res;
+  int res;
 	char fpath[1000];
-	char newFile[1000];
-	if (strcmp(path, "/") != 0) {
-		memcpy(newFile, path, strlen(path) - 9);
-		newFile[strlen(path) - 9] = '\0';
-	} else {
-		memcpy(newFile, path, strlen(path));
-	}
-
-	sprintf(fpath,"%s%s",dirpath,newFile);
-	printf("full path: %s\n", fpath);
+	sprintf(fpath,"%s%s",dirpath,path);
 	res = lstat(fpath, stbuf);
 
 	if (res == -1)
@@ -35,8 +26,7 @@ static int xmp_getattr(const char *path, struct stat *stbuf)
 static int xmp_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
 		       off_t offset, struct fuse_file_info *fi)
 {
-	printf("xmp_readdir: %s\n", path);
-	char fpath[1000];
+  char fpath[1000];
 	if(strcmp(path,"/") == 0)
 	{
 		path=dirpath;
@@ -62,7 +52,6 @@ static int xmp_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
 		memset(&st, 0, sizeof(st));
 		st.st_ino = de->d_ino;
 		st.st_mode = de->d_type << 12;
-		printf("de->d_name: %s\n", de->d_name);
 		res = (filler(buf, newName, &st, 0));
 			if(res!=0) break;
 	}
@@ -74,22 +63,15 @@ static int xmp_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
 static int xmp_read(const char *path, char *buf, size_t size, off_t offset,
 		    struct fuse_file_info *fi)
 {
-	printf("xmp_read: %s", path);
-	char fpath[1000];
-	char newFile[1000];
+  char fpath[1000];
 	if(strcmp(path,"/") == 0)
 	{
-		memcpy(newFile, path, strlen(path));
-		//path=dirpath;
+		path=dirpath;
 		sprintf(fpath,"%s",path);
 	}
-	else {
-		memcpy(newFile, path, strlen(path) - 9);
-		newFile[strlen(path)-9] = '\0';
-		sprintf(fpath, "%s%s",dirpath,newFile);
-	}
+	else sprintf(fpath, "%s%s",dirpath,path);
 	int res = 0;
-	int fd = 0 ;
+  int fd = 0 ;
 
 	(void) fi;
 	fd = open(fpath, O_RDONLY);
@@ -115,3 +97,4 @@ int main(int argc, char *argv[])
 	umask(0);
 	return fuse_main(argc, argv, &xmp_oper, NULL);
 }
+
